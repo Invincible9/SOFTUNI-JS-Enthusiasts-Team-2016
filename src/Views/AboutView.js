86dfb350aa8ee50c463aa38c78d6d4a1eb698e2b
TeodorDimitrov89 <braveheart1989@abv.bg>
2016-12-03 14:48:31 +0200

    delete all old files

--- src/Views/AboutView.js
+++
@@ -1,8 +0,0 @@
-import React,{Components} from 'react';
-
-export default function AboutPage() {
-    return <div className="about-view">
-        <h1>About Team</h1>
-    </div>
-}
-
--- src/Views/HomeView.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function HomeView() {
-    return <div className="home-view">
-        <h1>Welcome home</h1>
-    </div>
-}
